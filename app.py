0a3f925941bbc3b0bcd8efdf51d847b06560c874
hitoshi <180441055@ccmailg.meijo-u.ac.jp>
2021-01-21 17:35:52 +0900

    テンプレート作成

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def hello_world():
+    return render_template('bootstrap_html.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
